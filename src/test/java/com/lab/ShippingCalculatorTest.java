d4fe4dafc8c909c1364dc35c2d74f5867b75f60a
nguyentrongnhan <25020301@vnu.edu.vn>
2026-05-04 23:23:44 +0700

    Bai10

--- src/test/java/com/lab/ShippingCalculatorTest.java
+++ src/test/java/com/lab/ShippingCalculatorTest.java
@@ -2,7 +2,7 @@ package com.lab;
 
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.*
+import static org.junit.jupiter.api.Assertions.*;
 
 public class ShippingCalculatorTest {
 
